ee948caa3a40ac93ef102962b1d0bedf5d3f4338
sergiocohens <sergiocohensalama@pursuit.org>
2019-11-04 20:30:37 -0500

    complete

--- apis-express-postgres/client/index.js
+++ apis-express-postgres/client/index.js
@@ -5,12 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const postsByIdButton = document.querySelector('#postsByIdButton')
     const postButton = document.querySelector('#postsButton')
     const likesOfPostButton = document.querySelector('#likesOfPostButton')
+    const usersThatLikedButton = document.querySelector('#usersThatLikedButton')
+    const numOfLikesButton = document.querySelector('#numOfLikesButton')
+
     userForm.addEventListener('submit', addUserFormSubmitted)
     userButton.addEventListener('click', () => loadUsers())
     postForm.addEventListener('submit', addPostFormSubmitted)
     postsByIdButton.addEventListener('click', () => loadPostsById())
     postButton.addEventListener('click', () => loadPosts())
     likesOfPostButton.addEventListener('click', () => likesOfPost())
+    usersThatLikedButton.addEventListener('click', () => usersThatLiked())
+    numOfLikesButton.addEventListener('click', () => numOfLikes())
 });
 
 async function loadUsers() {
@@ -70,4 +75,28 @@ async function likesOfPost() {
     const postId = document.querySelector('#postIdInput').value
     const response = await axios.get(`http://localhost:3030/likes/${postId}`)
     likes.innerText = response.data.payload[0].count
+}
+
+async function usersThatLiked() {
+    const usersThatLiked = document.querySelector('#usersThatLiked')
+    usersThatLiked.innerText = ""
+    const postId2 = document.querySelector('#postIdInput2').value
+    const response = await axios.get(`http://localhost:3030/likes/liked/${postId2}`)
+    response.data.payload.forEach((user) => {
+      let listItem = document.createElement('li')
+      listItem.innerText = `${user.firstname} ${user.lastname}, age ${user.age}`
+      usersThatLiked.appendChild(listItem)
+    })
+}
+
+async function numOfLikes() {
+    const numOfLikes = document.querySelector('#numOfLikes')
+    numOfLikes.innerText = ''
+    const number = document.querySelector('#numOfLikesInput').value
+    const response = await axios.get(`http://localhost:3030/likes/num/${number}`)
+    response.data.payload.forEach((post) => {
+      let listItem = document.createElement('li')
+      listItem.innerText = `${post.id} ${post.body}`
+      numOfLikes.appendChild(listItem)
+    })
 }
--- apis-express-postgres/server/routes/likesRouter.js
+++ apis-express-postgres/server/routes/likesRouter.js
@@ -12,7 +12,7 @@ router.get('/:post_id', async (req,res) => {
     let number = await db.any(`SELECT COUNT(post_id) FROM likes JOIN posts ON likes.post_id = posts.id WHERE likes.post_id = ${postId}`)
     res.json({
       payload: number,
-      message: 'Retrieved posts'
+      message: 'Retrieved likes'
     })
   } catch(error) {
     res.status(500)
@@ -23,6 +23,38 @@ router.get('/:post_id', async (req,res) => {
   }
 })
 
+router.get('/liked/:liked_id', async (req,res) => {
+  let likedId = req.params.liked_id
+  try {
+    let users = await db.any(`SELECT DISTINCT firstname, lastname, age FROM users JOIN posts ON users.id = posts.poster_id JOIN likes ON users.id = likes.liker_id WHERE likes.post_id = ${likedId}`)
+    res.json({
+      payload: users,
+      message: 'Retrieved users'
+    })
+  } catch(error) {
+    res.status(500)
+    res.json({
+        message: 'There was an error.'
+    })
+    console.log('Error:', error)
+  }
+})
 
+router.get('/num/:number', async (req,res) => {
+  let num = req.params.number
+  try {
+    let posts = await db.any(`SELECT posts.id, posts.body, COUNT(posts.id) AS likes FROM posts JOIN likes ON posts.id = likes.post_id GROUP BY posts.id HAVING COUNT(posts.id) = ${num}`)
+    res.json({
+      payload: posts,
+      message: 'Retrieved users'
+    })
+  } catch(error) {
+    res.status(500)
+    res.json({
+        message: 'There was an error.'
+    })
+    console.log('Error:', error)
+  }
+})
 
 module.exports = router
